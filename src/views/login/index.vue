a1fca06b0bdf695322ea2152fee8e7bbab8aecf1
Eventidus <1024jianghu@gmail.com>
2023-11-19 18:03:06 +0800

    Implemented form validation rules

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,18 +4,18 @@
     <div class="form">
       <h1>登录</h1>
       <el-card shadow="never" class="login-card">
-        <el-form>
-          <el-form-item>
-            <el-input placeholder="手机号" />
+        <el-form ref="form" :model="loginForm" :rules="loginRules">
+          <el-form-item prop="mobile">
+            <el-input v-model="loginForm.mobile" placeholder="手机号" />
+          </el-form-item>
+          <el-form-item prop="password">
+            <el-input v-model="loginForm.password" show-password placeholder="密码" />
+          </el-form-item>
+          <el-form-item prop="isAgree">
+            <el-checkbox v-model="loginForm.isAgree">用户平台使用协议</el-checkbox>
           </el-form-item>
           <el-form-item>
-            <el-input placeholder="密码" />
-          </el-form-item>
-          <el-form-item>
-            <el-checkbox>用户平台使用协议</el-checkbox>
-          </el-form-item>
-          <el-form-item>
-            <el-button style="width: 350px;" type="primary">登录</el-button>
+            <el-button style="width: 350px" type="primary" @click="login">登录</el-button>
           </el-form-item>
         </el-form>
       </el-card>
@@ -24,7 +24,57 @@
 </template>
 <script>
 export default {
-  name: 'Login'
+  name: 'Login',
+  data() {
+    return {
+      loginForm: {
+        mobile: '',
+        password: '',
+        isAgree: false
+      },
+      loginRules: {
+        mobile: [
+          {
+            required: true,
+            message: '手机号不能为空!',
+            trigger: 'blur'
+          },
+          {
+            pattern: /^1[3-9]\d{9}$/,
+            message: '手机号格式不正确!',
+            trigger: 'blur'
+          }
+        ],
+        password: [
+          {
+            required: true,
+            message: '密码不能为空',
+            trigger: 'blur'
+          },
+          {
+            min: 6,
+            max: 16,
+            message: '密码长度应为6-16位!',
+            trigger: 'blur'
+          }
+        ],
+        isAgree: [{
+          validator(rule, value, callback) {
+            value ? callback() : callback(new Error('请同意勾选用户协议'))
+          }
+        }]
+      }
+    }
+  },
+  methods: {
+    login() {
+      this.$refs.form.validate((isOK) => {
+        if (isOK) {
+          alert('通过')
+        }
+      })
+    }
+  }
 }
 </script>
 <style lang="scss">
@@ -32,23 +82,24 @@ export default {
   display: flex;
   align-items: stretch;
   height: 100vh;
+
   .logo {
     flex: 3;
-    background: rgba(38, 72, 176) url(../../assets/common/login_back.png)
-      no-repeat center / cover;
+    background: rgba(38, 72, 176) url(../../assets/common/login_back.png) no-repeat center / cover;
     border-top-right-radius: 60px;
     display: flex;
     flex-direction: column;
     align-items: flex-end;
     justify-content: center;
     padding: 0 100px;
+
     .icon {
-      background: url(../../assets/common/logo.png) no-repeat 70px center /
-        contain;
+      background: url(../../assets/common/logo.png) no-repeat 70px center / contain;
       width: 300px;
       height: 50px;
       margin-bottom: 50px;
     }
+
     p {
       color: #fff;
       font-size: 18px;
@@ -57,30 +108,35 @@ export default {
       text-align: center;
     }
   }
+
   .form {
     flex: 2;
     display: flex;
     flex-direction: column;
     justify-content: center;
     padding-left: 176px;
+
     .el-card {
       border: none;
       padding: 0;
     }
+
     h1 {
       padding-left: 20px;
       font-size: 24px;
     }
+
     .el-input {
       width: 350px;
       height: 44px;
+
       .el-input__inner {
         background: #f4f5fb;
       }
     }
+
     .el-checkbox {
-      color:#606266;
+      color: #606266;
     }
   }
-}
-</style>
+}</style>
